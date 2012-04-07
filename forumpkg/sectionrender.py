09d97eca3218806d9b24cf23cc316cd5bd8831ad
Klimov Kirill <ra22341@ya.ru>
2012-04-07 15:24:51 +0400

    fix bug with empty subsections

--- forumpkg/sectionrender.py
+++ forumpkg/sectionrender.py
@@ -6,7 +6,7 @@ def rendermssgs(config, db, tmpr, section , prnt_id=None):
    return res
 
 def rendersections(config, db, tmpr, prnt_id=None):
-   res = ''
+   res = '<h2>Sections:</h2>'
    sql = "select sct_name, description, sct_id from sections where fk_sct_prnt_id "
    if prnt_id == None:
       sql += "is NULL"
@@ -14,9 +14,12 @@ def rendersections(config, db, tmpr, prnt_id=None):
       sql += "=" + str(prnt_id)
    data = db.Run(sql)
 
-   for x in data:
-      tmp = tmpr.MkPageFromFile("templates/inner_templates/section_tmp.xml", {'section_name' : str(x[0]), 'section_desc' : str(x[1]), 'sect_href' :  'index.py?action=showsection&value=' + str(x[2]) })
-      res += tmp
-   '''if prnt_id != None:
-      res +=rendermssgs(config, db, tmpr, section, prnt_id)'''
+
+   if len(data) != 0:
+      for x in data:
+         tmp = tmpr.MkPageFromFile("templates/inner_templates/section_tmp.xml", {'section_name' : str(x[0]), 'section_desc' : str(x[1]), 'sect_href' :  'index.py?action=showsection&value=' + str(x[2]) })
+         res += tmp 
+   else:
+      res += 'No sections'
+
    return res
