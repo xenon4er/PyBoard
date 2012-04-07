a52b63b900e2de2f506e570d4d6ef872be3cb6af
Klimov Kirill <ra22341@ya.ru>
2012-04-07 14:08:42 +0400

    New sect render func

--- forumpkg/sectionrender.py
+++ forumpkg/sectionrender.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-def rendersections(config,db,tmpr):
+def rendersections(config, db, tmpr, prnt_id=None):
    res = ''
-   data = db.Run("select sct_name, description, sct_id from sections where fk_sct_prnt_id is NULL")
+   sql = "select sct_name, description, sct_id from sections where fk_sct_prnt_id "
+   if prnt_id == None:
+      sql += "is NULL"
+   else:
+      sql += "=" + str(prnt_id)
+   data = db.Run(sql)
 
    for x in data:
       tmp = tmpr.MkPageFromFile("templates/inner_templates/section_tmp.xml", {'section_name' : str(x[0]), 'section_desc' : str(x[1]), 'sect_href' :  'index.py?showsection=' + str(x[2]) })
