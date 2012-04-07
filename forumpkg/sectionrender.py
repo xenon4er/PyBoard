7a556dec0d4e20b1d463d087908044ef9e712c6e
Klimov Kirill <ra22341@ya.ru>
2012-04-07 13:41:13 +0400

    add section w

--- forumpkg/sectionrender.py
+++ forumpkg/sectionrender.py
@@ -3,8 +3,10 @@
 
 def rendersections(config,db,tmpr):
    res = ''
-   data = db.Run("select * from sections")
+   data = db.Run("select sct_name, description, sct_id from sections where fk_sct_prnt_id is NULL")
+
    for x in data:
-      res += str(x)
+      tmp = tmpr.MkPageFromFile("templates/inner_templates/section_tmp.xml", {'section_name' : str(x[0]), 'section_desc' : str(x[1]), 'sect_href' :  'index.py?showsection=' + str(x[2]) })
+      res += tmp
 
    return res
