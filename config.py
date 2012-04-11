fcdf0bb5f854cb292efcbc342bb35a1e9082c65b
Klimov Kirill <ra22341@ya.ru>
2012-04-11 23:50:09 +0400

    dels *.pyc

--- config.py
+++ config.py
@@ -6,8 +6,8 @@
 conf = {}
 
 conf ['dbuser'] = 'root'
-conf ['dbpassword'] = 'www22341'
+conf ['dbpassword'] = 'pass'
 conf ['dbhost'] = 'localhost'
-conf ['dbname'] = 'klimov_k_v'
+conf ['dbname'] = 'ForumDB'
 conf ['debug'] = True
 conf ['dbprefix'] = ''
